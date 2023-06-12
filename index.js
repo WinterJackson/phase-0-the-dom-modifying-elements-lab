8216db50715580f83e4bcd73169cd954e32681a2
WinterJackson <winterjacksonwj@gmail.com>
2023-06-12 15:04:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector('main#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const yourName = 'John Winter'; 
+newHeader.textContent = `${yourName} is the champion`;
